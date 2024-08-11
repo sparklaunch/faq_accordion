3ee03f1035065a6b562698942886163b9bceda94
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2024-08-11 09:58:31 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/faq_accordion/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.faq_accordion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:faq_accordion/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: MainScreen(),
+      ),
+    ),
+  );
+}
